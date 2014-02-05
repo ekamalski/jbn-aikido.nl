489b71c0c93e822d1b98a6e52fe69b840172f12d
ekamalski <ekamalski@openmailbox.org>
2014-02-05 15:55:51 +0100

    Refactor textlib.php

--- include/textlib.php
+++ include/textlib.php
@@ -1,16 +1,155 @@
 <?php
-// Ident: 2012-12-26
-function dekalender() {
-	print("<a href='?page=kalender'>kalender</a>\n");
+// Ident: 2014-02-05
+
+define("BENNEDETTI",   "Stefan Bennedetti");
+define("BENNEDETTI_T", "bennedetti_t.jpg");
+
+define("CLAUDE",   	"Claude Pellerin");
+define("CLAUDE_T",  "claude_t.jpg");
+
+define("FUKAKUSA",   "Fukakusa sensei");
+define("FUKAKUSA_T", "fukakusa_t.jpg");
+
+define("DONOVAN",    "Donovan Waite");
+define("DONOVAN_T",  "donovan_t.jpg");
+
+define("MICHAEL",    "Michael Martin");
+define("MICHAEL_T",  "mickael_t.jpg");
+
+define("TIKI",       "Malcolm Tiki Shewan");
+define("TIKI_T",     "tiki-shewan_t.jpg");
+
+define("AMERSFOORT",   "Amersfoort");
+define("AMERSFOORT_L", "Furglerplein 3, Amersfoort");
+
+define("GENNEP",       "Gennep");
+define("GENNEP_L",     "Picardie 36, Gennep");
+
+define("NIJMEGEN",     "Nijmegen");
+define("NIJMEGEN_L",   "Tapirstraat 2, 6532 AL Nijmegen");
+
+define("OOSRERHOUT",   "Oosterhout");
+define("OOSRERHOUT_L", "Slotjesveld 9, 4902 AA Oosterhout");
+
+function examens($datum, $lokatie, $txt1) {
+	if ($datum != "") {
+		h2("Danexamens", $datum, $lokatie);
+		if ($txt1 != "" ) {
+			print("$txt1</br>\n");
+		}
+		examensDetails();
+	}
 }
 
-function detailsTeVinden() {
-	print("De details zijn te vinden in de\n");
+function nieuwsitemLink($ref,$item) {
+	print("<h2 style='clear: both;'>Nieuws item: $item</h2>\n");
+	print("Klik op de link <a href='?page=nieuws&nieuwsItem=$ref'>\n$item</a> om het hele nieuws item te lezen\n");
+}
+
+function nieuwsitem($title, $ref) {
+	print("<h2 style='clear: both;'>Nieuws item: $title</h2>\n");
+	require_once(ABSPATH."/nieuws/$ref");
+}
+
+
+function stage($leraren, $datum, $lokatie, $thumb="", $txt1="", $txt2="", $ref="") {
+	if ($datum != "") {
+		h2($leraren, $datum, $lokatie);
+		if ($thumb == "") {
+			stageWithoutThumb($txt1, $ref);
+		} else {
+			stageWithThumb($thumb, $txt1, $txt2, $ref);
+		}
+	}
+}
+
+function jan($year, $day1, $day2=0, $day3=0) {
+	$days = getDays($day1, $day2, $day3);
+	return "$days januari";
+}
+
+function feb($year, $day1, $day2=0, $day3=0) {
+	$days = getDays($day1, $day2, $day3);
+	return "$days februari";
+}
+
+function mar($year, $day1, $day2=0, $day3=0) {
+	$days = getDays($day1, $day2, $day3);
+	return "$days maart";
+}
+
+function apr($year, $day1, $day2=0, $day3=0) {
+	$days = getDays($day1, $day2, $day3);
+	return "$days april";
+}
+
+function may($year, $day1, $day2=0, $day3=0) {
+	$days = getDays($day1, $day2, $day3);
+	return "$days mei";
+}
+
+function jun($year, $day1, $day2=0, $day3=0) {
+	$days = getDays($day1, $day2, $day3);
+	return "$days juni";
+}
+
+function jul($year, $day1, $day2=0, $day3=0) {
+	$days = getDays($day1, $day2, $day3);
+	return "$days juli";
+}
+
+function aug($year, $day1, $day2=0, $day3=0) {
+	$days = getDays($day1, $day2, $day3);
+	return "$days augustus";
+}
+
+function sep($year, $day1, $day2=0, $day3=0) {
+	$days = getDays($day1, $day2, $day3);
+	return "$days september";
+}
+
+function oct($year, $day1, $day2=0, $day3=0) {
+	$days = getDays($day1, $day2, $day3);
+	return "$days oktober";
+}
+
+function nov($year, $day1, $day2=0, $day3=0) {
+	$days = getDays($day1, $day2, $day3);
+	return "$days november";
+}
+
+function dec($year, $day1, $day2=0, $day3=0) {
+	$days = getDays($day1, $day2, $day3);
+	return "$days december";
+}
+
+// Private
+
+function getDays($day1, $day2, $day3) {
+	$days = $day1;
+	if ($day2 != 0) { $days = "$days, $day2"; }
+	if ($day3 != 0) { $days = "$days, $day3"; }
+	return $days;
+}
+function trc($txt){
+	print("TRC, $txt");
+}
+
+function brString($text) {
+	return "$text<br/>\n";
+}
+
+function dekalender() {
+	print("<a href='?page=kalender'>kalender</a>\n");
 }
 
 function details() {
 	detailsTeVinden();
 	dekalender();
+}
+
+function detailsTeVinden() {
+	print("De details zijn te vinden in de\n");
 }
 
 function examensDetails() {
@@ -22,101 +161,47 @@ function h2($leraren, $datum, $lokatie) {
 	print("<h2 style='clear: both;'>$leraren, $datum, $lokatie</h2>\n");
 }
 
-function imgRef($ref, $thumb) {
-	print("<a style='float: left;' href='events/$ref'>\n<img src='events/$thumb'></a>\n");
+function imgTextThumb($thumb, $txt1="", $ref="") {
+	if ($ref == "" ) {
+		print("<img style='float: left;' src='events/$thumb'>");
+	} else {
+		print("<a style='float: left;' href='events/$ref'>\n<img src='events/$thumb'></a>\n");
+	}
+	print("<div style='float: left; padding-left: 10px;'>\n$txt1");
+	print("</div>\n");
+	print("<p style='clear: both;'>\n");
+}
+
+function stageWithoutThumb($txt1="", $ref="") {
+	if ($txt1 != "" ) {
+		print("$txt1</br>\n");
+	}
+	
+	detailsTeVinden();	
+	if ($ref != "") {		
+		txtRef($ref, "PDF-flyer");
+	}
+	dekalender();
+}
+
+function stageWithThumb($thumb, $txt1="", $txt2="", $ref="") {
+	imgTextThumb($thumb, $txt1, $ref);
+		
+	if ($txt2 != null || "$txt2" != "") {
+		print("<br/>$txt2<br/>");
+	}
+	print("<br/>");
+	
+	detailsTeVinden();	
+	if ($ref != "") {		
+		txtRef($ref, "PDF-flyer");
+	}
+	dekalender();
 }
 
 function txtRef($ref, $txt) {
 	print("<a href='events/$ref'>\n$txt</a> en de \n");
 }
-
-// --- templates ---
-function nieuwsitemLink($ref,$item) {
-	print("<h2 style='clear: both;'>Nieuws item: $item</h2>\n");
-	print("Klik op de link <a href='?page=nieuws&nieuwsItem=$ref'>\n$item</a> om het hele nieuws item te lezen\n");
-}
-
-function nieuwsitem($title, $ref) {
-	print("<h2 style='clear: both;'>Nieuws item: $title</h2>\n");
-	require_once(ABSPATH."/nieuws/$ref");
-}
-
-function eenvoudig($leraren, $datum, $lokatie, $txt1) {
-	h2($leraren, $datum, $lokatie);
-	if ($txt1 != "" ) {
-		print("$txt1</br></br>\n");
-	}
-	details();
-}
-
-function examens($datum, $lokatie, $txt1) {
-	h2("Danexamens", $datum, $lokatie);
-	if ($txt1 != "" ) {
-		print("$txt1</br>\n");
-	}
-	examensDetails();
-}
-
-function eenvoudigGeenDetails($leraren, $datum, $lokatie, $txt1) {
-	h2($leraren, $datum, $lokatie);
-	if ($txt1 != "" ) {
-		print("$txt1</br>\n");
-	}
-}
-
-function pdf($leraren, $datum, $lokatie, $ref, $txt1) {
-	h2($leraren, $datum, $lokatie);
-	if ($txt1 != "" ) {
-		print("$txt1</br></br>\n");
-	}
-	detailsTeVinden();
-	txtRef($ref, "PDF-flyer");
-	dekalender();
-	print("</p>\n");
-}
-
-function thumb($leraren, $datum, $lokatie, $thumb, $txt1) {
-	h2($leraren, $datum, $lokatie);
-	print("<img style='float: left;' src='events/$thumb'>");
-	print("<div style='float: left; padding-left: 10px;'>\n");
-	if ($txt1 != null || "$txt1" != "") {
-		print("$txt1<br/><br/>");
-	}
-	detailsTeVinden();
-	// ....
-	dekalender();
-	print("</div>\n");
-	print("<p style='clear: both;'><br/><br/>");
-}
-
-
-
-function thumbPdf($leraren, $datum, $lokatie, $ref, $thumb, $txt1, $txt2) {
-	h2($leraren, $datum, $lokatie);
-	imgRef($ref, $thumb);
-	print("<div style='float: left; padding-left: 10px;'>\n$txt1");
-	print("</div>\n");
-	print("<p style='clear: both;'>\n");
-	if ($txt2 != null || "$txt2" != "") {
-		print("<br/>$txt2<br/>");	
-	}
-	print("<br/>");
-	detailsTeVinden();
-	txtRef($ref, "PDF-flyer");
-	dekalender();
-	print("</p>\n");
-}
-
-function thumbPdf2($leraren, $datum, $lokatie, $ref, $thumb, $txt1, $txt2) {
-	h2($leraren, $datum, $lokatie);
-	imgRef($ref, $thumb);
-	print("<div style='float: left; padding-left: 10px;'>\n$txt1<br/><br/>");
-	print("<br/><br/></div>\n");
-	print("<p style='clear: both;'>\n");
-	detailsTeVinden();
-	txtRef($ref, "PDF-flyer");
-	dekalender();
-    print("<br/>$txt2");
-	print("</p>\n");
-}
+	
+	
 ?>
--- pages/home.php
+++ pages/home.php
@@ -3,50 +3,22 @@
 
 require_once(DOCROOT."/include/textlib.php");
 
-thumb("Claude Pellerin", "8 en 9 februari", "Beek", "claude_t.jpg", 
-"zaterdag 8 februari, 15:00 - 18:00<br/>"
-."zondag 9 februari, 9:00 - 12:00<br/>"
-."Markt 6a, Beek<br/>"
-."Kosten: 20.00 euro"
+stage(CLAUDE, feb(2014, 8, 9), "Beek", CLAUDE_T,
+brString("zaterdag 8 februari, 15:00 - 18:00")
+.brString("zondag 9 februari, 9:00 - 12:00")
+.brString("Markt 6a, Beek")
+.brString("Kosten: 20.00 euro")
 );
 
-eenvoudig("Dan examens", "2 maart", "Amersfoort", 
-"Vanaf 9,00<br/>"
-."Furglerplein 3, Amersfoort"
+examens(mar(2014, 2), AMERSFOORT, 
+brString("Vanaf 9,00")
+.brString(AMERSFOORT_L)
 );
 
-thumb("Fukakusa sensei", "17 en 18 mei", "Gennep", "fukakusa_t.jpg", 
-"Picardie 36, Gennep");
 
-/*
-thumbPdf("Michael Martin", "22, 23 en 24 November", "Gennep/Nijnegen", 
-"2013-11-22_mickael_martin.jpg", "mickael_t.jpg", 
-"vrijdag 22 november, 20:00 - 21:30<br/>"
-."Picardie 36, 6591 JE Gennep<br/>"
-."<br/>"
-."zaterdag, 23 november, 15:00 - 18:00<br/>"
-."zondag, 24 november, 09:00 - 12:00<br/>"
-."Tapirstraat 2, 6532 AL Nijmegen<br/>"
-."<br/>"
-."JBN leden<br/>"
-."Gennep: 7.50<br/>"
-."Nijmegen: 15.00 per dag, weekend: 27.50"
-,
-"Niet leden<br/>"
-."Gennep: 8.50<br/>"
-."Nijmegen: 17.50 per dag, weekend: 30.00"
-
+stage(FUKAKUSA, may(2014, 17, 18), GENNEP, FUKAKUSA_T, 
+GENNEP_L
 );
 
-thumbPdf("Malcolm Tiki Shewan", "23 en 24 november", "Oosterhout", 
-"2013-11-23_tiki_shewan.pdf", "tiki-shewan_t.jpg", 
-"zaterdag 23 november, 15:00 - 18:00<br/>"
-."zondag 24 november, 10:00 - 13:00<br/>"
-."Slotjesveld 9, 4902 AA OOsterhout<br/>"
-."<br/>"
-."17.50 per dag, weekend: 35.00"
-,
-null);
-*/
 ?>
 
