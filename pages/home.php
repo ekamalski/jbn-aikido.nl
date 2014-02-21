55d82a0c027fd5f8e4e0ff313317375a6ede4233
ekamalski <ekamalski@openmailbox.org>
2014-02-21 11:35:37 +0100

    21-feb

--- pages/home.php
+++ pages/home.php
@@ -3,12 +3,9 @@
 
 require_once(DOCROOT."/include/textlib.php");
 
-stage(CLAUDE, feb(2014, 8, 9), "Beek", CLAUDE_T,
-brString("zaterdag 8 februari, 15:00 - 18:00")
-.brString("zondag 9 februari, 9:00 - 12:00")
-.brString("Markt 6a, Beek")
-.brString("Kosten: 20.00 euro")
-);
+// stage(CLAUDE, feb(2014, 8, 9), "Beek", CLAUDE_T,
+// brString("zaterdag 8 februari, 15:00 - 18:00")
+// );
 
 examens(mar(2014, 2), AMERSFOORT, 
 brString("Vanaf 9,00")
