80539c7c57e8ccca75e2bef34063ad9cf9030c54
ekamalski <jbn-aikido.webmaster@aikidoarnhem.nl>
2015-02-23 14:38:07 +0100

    2015-02-23_14.38

---
+++ include/defslib.php
@@ -0,0 +1,87 @@
+<?php
+
+define("NO_T", 			""); // no thumb.
+
+define("BENNEDETTI",    "Stefan Bennedetti");
+define("BENNEDETTI_T",  "bennedetti_t.jpg");
+
+define("CAREL",   	    "Carel Zappeij");
+define("CAREL_T",       "carel_t.jpg");
+
+define("CLAUDE",   	    "Claude Pellerin");
+define("CLAUDE_T",      "claude_t.jpg");
+
+define("CURTIS",   	    "Chris Curtis");
+define("CURTIS_T",      "curtis_t.jpg");
+
+define("DONOVAN",       "Donovan Waite");
+define("DONOVAN_T",     "donovan_t.jpg");
+
+define("FLOQUET",       "Alain Floquet sensei 9e dan");
+define("FLOQUET_T",     "floquet_t.jpg");
+
+define("FUKAKUSA",      "Fukakusa 8ste dan shihan");
+define("FUKAKUSA_T",    "fukakusa_t.jpg");
+
+define("IAS",           "IAS");
+define("IAS_T",         "ias_t.jpg");
+
+define("MICKAEL",       "Mickael Martin");
+define("MICKAEL_T",     "mickael_t.jpg");
+
+define("MIMURO",        "Mimuro 7de dan shihan");
+define("MIMURO_T",      "mimuro_t.jpg");
+
+define("PIERRE",        "Pierre Geraedts");
+define("PIERRE_T",      "pierre_t.jpg");
+
+define("ROINEL",        "Alain Roinel");
+define("ROINEL_T",      "roinel_t.jpg");
+
+define("TIKI",          "Malcolm Tiki Shewan");
+define("TIKI_T",        "tiki-shewan_t.jpg");
+
+define("YUKIHIRO",      "Sugino Yukihiro sensei");
+define("YUKIHIRO_T",    "yukihiro_t.jpg");
+
+define("ALLARDSOOG",    "Allardsoog");
+define("ALLARDSOOG_L",  "Jarig van de Wielenwei 42, 9343 TC Een-West");
+
+define("AMERSFOORT",    "Amersfoort");
+define("AMERSFOORT_L",  "Furglerplein 3, Amersfoort");
+
+define("GENNEP",        "Gennep");
+define("GENNEP_L",      "Picardie 36, Gennep");
+
+define("HEERENVEEN",    "Heerenveen");
+define("HEERENVEEN_L",  "Abe Lenstra boulevard 23Q, 8448 JA Heerenveen");
+
+define("HELMOND",       "Helmond");
+define("HELMOND_L",     "Barrierlaan 41, Helmond");
+
+define("NIEUWEGEIN",    "Nieuwegein");
+define("NIEUWEGEIN_L",  "Blokhoeve 5, Nieuwegein");
+
+define("NIJMEGEN",      "Nijmegen");
+define("NIJMEGEN_L",    "Tapirstraat 2, 6532 AL Nijmegen");
+
+define("LELYSTAD",      "Lelystad");
+define("LELYSTAD_L",    "Sportcentrum De Koploper, Badweg 21, 8223 PA Lelystad");
+
+define("OOSTERHOUT",    "Oosterhout");
+define("OOSTERHOUT_L",  "Slotjesveld 9, 4902 AA Oosterhout");
+
+define("ROERMOND",      "Roermond");
+define("ROERMOND_L",    "Prins Bernardstraat 1, Roermond");
+
+define("STROE",         "Stroe");
+define("STROE_L",       "Tolnegenweg 53, 3776 PV Stroe");
+
+define("VEGHEL",        "Veghel");
+define("VEGHEL_L",      "Prins Willem Alexander Sportpark 8, 5461 XL Veghel");
+
+define("WIJK_BIJ_DUURSTEDE",   "Wijk bij Duurstede");
+define("WIJK_BIJ_DUURSTEDE_L", "Lekdijk oost 13a, Wijk bij Duurstede");
+
+
+?>
